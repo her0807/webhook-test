0c0c10c82f8a51dceeca2cdb8d69e961ed247000
JUDONGHYEOK <judonghyeok@gmail.com>
2024-03-22 22:11:59 +0900

    feat

--- .github/actions/get-variables/action.js
+++ .github/actions/get-variables/action.js
@@ -30,6 +30,28 @@ try {
   // const { lang, service } = ENV_REGION_MAP[env][region];
 
   // console.log(JSON.stringify(github));
+
+  const data = JSON.stringify({
+    data: 'hello',
+  });
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+    },
+    body: {
+      payload: JSON.stringify({
+        text: 'Hello, World!',
+      }),
+    },
+  };
+  const req = http.request(region, options, (res) => {
+    console.log(res.body);
+  });
+
+  req.end(data);
+
   console.log(region);
   core.setOutput('service', region);
 } catch (error) {

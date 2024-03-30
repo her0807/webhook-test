50d2f4cb416c4d336f01a8bfde5f504fbef2e431
주동혁 <judonghyeok@woowahan.com>
2024-03-30 14:21:46 +0900

    t

--- .github/actions/request_review/request_review.js
+++ .github/actions/request_review/request_review.js
@@ -1,36 +1,36 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { IncomingWebhook } = require('@slack/webhook');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { IncomingWebhook } = require("@slack/webhook");
 
 const USERS = [
   {
-    slackID: 'U06QSKJDCF7',
-    githubID: 'JUDONGHYEOK',
+    slackID: "U06QSKJDCF7",
+    githubID: "JUDONGHYEOK",
   },
-  { slackID: 'U06QSKJDCF7', githubID: 'KoreanDonkey' },
+  { slackID: "U06QSKJDCF7", githubID: "KoreanDonkey" },
 ];
 
 try {
-  const url = core.getInput('slack_url');
+  const url = core.getInput("slack_url");
   const webhook = new IncomingWebhook(url);
 
   const send = async () => {
     webhook.send(
       {
-        text: 'PR이 도착했습니다.🫡',
+        text: "PR이 도착했습니다.🫡",
         blocks: [
           {
-            type: 'header',
+            type: "header",
             text: {
-              type: 'plain_text',
-              text: 'PR이 도착했습니다.🫡',
+              type: "plain_text",
+              text: "PR이 도착했습니다.🫡",
               emoji: true,
             },
           },
           {
-            type: 'section',
+            type: "section",
             text: {
-              type: 'mrkdwn',
+              type: "mrkdwn",
               text: `<@${
                 USERS.find((user) => user.githubID === github.context.actor)
                   .slackID
@@ -38,37 +38,35 @@ try {
             },
           },
           {
-            type: 'section',
+            type: "section",
             text: {
-              type: 'mrkdwn',
+              type: "mrkdwn",
               text: `${github.context.payload.pull_request.requested_reviewers
                 .map((reviewer) => {
                   const slackID = USERS.find(
-                    (user) => user.githubID === reviewer.login
+                    (user) => user.githubID === reviewer.login,
                   )?.slackID;
                   return slackID ? `<@${slackID}>` : undefined;
                 })
                 .filter(Boolean)
-                .join(' ')}님 리뷰해주세요!`,
+                .join(" ")}님 리뷰해주세요!`,
             },
-          },{
-            type: 'section',
+          },
+          {
+            type: "section",
             text: {
-              type: 'mrkdwn',
+              type: "mrkdwn",
               text: `### PR 정보 \n - 제목: ${github.context.payload.pull_request.title} \n - URL: ${github.context.payload.pull_request.html_url}`,
             },
           },
-          {type: 'section',
-          text:{
-            type:'actions',
-            text:{
-              type:'button',
-              url: `${github.context.payload.pull_request.html_url}`,
-              text: 'PR 확인하기'
-            }
-          }
-        }
-
+          {
+            type: "actions",
+              text: {
+                type: "button",
+                url: `${github.context.payload.pull_request.html_url}`,
+                text: "PR 확인하기",
+              },
+          },
 
           // {
           //   color: '#36a64f',
@@ -92,7 +90,7 @@ try {
       },
       function (err, response) {
         console.log(response);
-      }
+      },
     );
   };
   console.log(github.context.payload.pull_request);
